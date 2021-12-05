4f391f73073f32eb45eda7f5b3f2d4cfea344ce3
Your Name <you@exaple.com>
2021-12-05 10:51:20 +0100

    zad.2 zrobione

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,34 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+        Student student1 = new Student();
+        student1.imie = "Adam";
+        student1.nazwisko = "Borek";
+        student1.id = 1;
+        student1.men = true;
+
+        Student student2 = new Student();
+        student2.imie = "Kamil";
+        student2.nazwisko = "Dąb";
+        student2.id = 2;
+        student2.men = true;
+
+        Student student3 = new Student();
+        student3.imie = "Alicja";
+        student3.nazwisko = "Borek";
+        student3.id = 3;
+        student3.men = false;
+        Student[] studenci = new Student[3];
+            studenci[0]=student1;
+            studenci[1]=student2;
+            studenci[2]=student3;
+            for (int i=0; i<3;i++) {
+                System.out.println(studenci[i].imie);
+                System.out.println(studenci[i].nazwisko);
+                System.out.println(studenci[i].id);
+                if(studenci[i].men){ System.out.println("mężczyzna");}else
+                    System.out.println("kobieta");
+                System.out.println();
+            }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+        String imie;
+        String nazwisko;
+        int id;
+        boolean men;
+
+        }
