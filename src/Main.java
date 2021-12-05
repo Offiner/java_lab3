3a52d5d679e47c3124b4992998bbfa5aeeb1a93b
Your Name <you@exaple.com>
2021-12-05 09:58:20 +0100

    zad.1 zrobione

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,66 @@ public class Main {
         /* statusy i wybór ze switch case to najczęstsze użycia enumów, enumy są często używane
         * w różnego rodzaju listach, selectach jako krótkie opcje wyboru np. jako status A, X (Aktualny, Usunięty) */
 
+        enum LiczbyEnum{jeden, dwa, trzy, cztery, pięć, sześć}
+        enum StatusEnum{KONTYNUUJEMY, KONIEC}
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj liczbę iteracji:");
+        int i = scan.nextInt();
+        while (i>0)
+         {
+
+            System.out.println("Wpisz liczbę");
+            switch(scan.nextInt()) {
+
+                case 1:
+                    System.out.println(LiczbyEnum.jeden);
+
+                    break;
+                case 2:
+                    System.out.println(LiczbyEnum.dwa);
+
+                    break;
+                case 3:
+                    System.out.println(LiczbyEnum.trzy);
+
+                    break;
+                case 4:
+                    System.out.println(LiczbyEnum.cztery);
+
+                    break;
+                case 5:
+                    System.out.println(LiczbyEnum.pięć);
+
+                    break;
+                case 6:
+                    System.out.println(LiczbyEnum.sześć);
+
+                    break;
+                default:System.out.println(StatusEnum.KONIEC);
+
+            }
+             i--;
+            if(i==0){
+                System.out.println(StatusEnum.KONIEC);}else
+                System.out.println(StatusEnum.KONTYNUUJEMY);
+        }
+        }
+
+        /* Prosty enum */
+        enum KolorEnum {
+            CZERWONY, ZIELONY, NIEBIESKI;
+        }
+
+        /* enum z metodą i zmienną */
+        enum WielkoscEnum {
+            MALY(false), DUZY(true), SREDNI(false), OGROMNY(true);
+
+            boolean wielkosc;
+
+            WielkoscEnum(boolean czyDuzy) {
+                wielkosc = czyDuzy;
+            }
+        }
     }
-}
+
